afdc2624864bdf2f16e82334ecb384da8a07ae79
Tarun Pal <143593638+TarunPal0812@users.noreply.github.com>
2025-07-30 01:06:48 +0530

    complete the project

---
+++ src/pages/Home.jsx
@@ -0,0 +1,22 @@
+
+import AiTools from '../components/AiTools'
+import Footer from '../components/Footer'
+import Heor from '../components/Heor'
+import Navbar from '../components/Navbar'
+import Plan from '../components/Plan'
+import Testimonial from '../components/Testimonial'
+
+const Home = () => {
+  return (
+    <>
+    <Navbar/>
+    <Heor/>
+    <AiTools/>
+    <Testimonial/>
+    <Plan/>
+    <Footer/>
+    </>
+  )
+}
+
+export default Home
